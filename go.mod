3999bec1c2ea5ee2d2003bed3e497b9408f1670c
m <m@ms-MacBook-Pro.attlocal.net>
2024-01-27 19:28:41 -0500

    restructure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xytosis/learning-go-lib
 
 go 1.21.6
+
+require github.com/go-chi/chi/v5 v5.0.11 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/xytosis/learning-go-lib/db/dao"
+	"github.com/xytosis/learning-go-lib/resources"
+	"github.com/xytosis/learning-go-lib/services"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	// create daos
+	bookDao := dao.BookDao{}
+
+	// create services
+	bookService := services.BookingService{}
+
+	// create contexts
+	bookServiceCtx := services.BookingServiceContext{&bookDao}
+	bookingResourceCtx := resources.BookingResourceContext{r, &bookService, &bookServiceCtx}
+	healthcheckResourceCtx := resources.HealthcheckResourceContext{r}
+
+	// register resources
+	h := resources.HealthcheckResource{}
+	h.RegisterResource(healthcheckResourceCtx)
+	b := resources.BookingResource{}
+	b.RegisterResource(bookingResourceCtx)
+
+	http.ListenAndServe(":3000", r)
 }
